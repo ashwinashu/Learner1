0c27c51b52fd0c7ec5d4997596c724dae558f621
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 15:00:28 +0530

    Update cms.controller.js

--- api/cmsContent/cms.controller.js
+++ api/cmsContent/cms.controller.js
@@ -3,6 +3,15 @@ const { endConnection } = require("../../helpers/databaseConnection");
 const chalk = require("chalk");
 const config = require("../../config");
 
+const testing = async (req, res, next) => {
+ 
+};
+
+
+
+const addcontent = async (req, res, next) => {
+ 
+};
 
 module.exports = {
   testing,
