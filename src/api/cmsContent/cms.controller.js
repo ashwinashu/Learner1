98ea1e25a705a144c6819cd0b5531374a73c51b6
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 18:17:53 +0530

    Update cms.controller.js

--- src/api/cmsContent/cms.controller.js
+++ src/api/cmsContent/cms.controller.js
@@ -1,7 +1,7 @@
 const CmsContent = require("./cms.model");
 const { endConnection } = require("../../helpers/databaseConnection");
 const chalk = require("chalk");
-const config = require("../../config");
+const db = require("../../db");
 
 
 
