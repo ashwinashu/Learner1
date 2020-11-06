577f9f5e79944d2c7be4857831c80829f2172694
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 18:17:33 +0530

    Update cms.route.js

--- src/api/cmsContent/cms.route.js
+++ src/api/cmsContent/cms.route.js
@@ -1,7 +1,7 @@
 const rconst router = require("express").Router();
 const cmsContent = require("./cms.controller");
 
-const config = require("../../config");
+const db = require("../../db");
 
 rounter.route("/").get(cmsContent.show);
 router
