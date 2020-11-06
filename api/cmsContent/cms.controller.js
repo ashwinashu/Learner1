640440a45f2726eb30ab7c1be18d75879d3e49a6
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 13:14:38 +0530

    Create cms.controller.js

---
+++ api/cmsContent/cms.controller.js
@@ -0,0 +1,10 @@
+const CmsContent = require("./cms.model");
+const { endConnection } = require("../../helpers/databaseConnection");
+const chalk = require("chalk");
+const config = require("../../config");
+
+
+module.exports = {
+  testing,
+  getcontent
+};
