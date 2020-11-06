6311a142e0f07d07aeafa7441428194a933962ff
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 14:53:18 +0530

    Update cms.route.js

--- api/cmsContent/cms.route.js
+++ api/cmsContent/cms.route.js
@@ -3,5 +3,8 @@ const cmsContent = require("./cms.controller");
 const config = require("../../config");
 
 rounter.route("/").get(cmsContent.testing);
+router
+  .route("/")
+  .put(cmsContent.getcontent);
 
 module.exports = router;
