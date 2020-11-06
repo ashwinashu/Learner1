20a536575cdc343c30919be0c70db3eef8dc403c
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-06 19:31:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: localhost,
+  database: learner1,
+  user: learner1,
+  password: learner@123,
+})
+
+app.get("/",(req,res)=>{
+  const squery = "INSERT INTO append (name) values ("Arun");"
+  db.query(squery , (err,result)=>{
+    res.send("Running ");
+  })
+})
 
 app.listen(3001,()=>{
-  console.log("Running in 3001");
+  console.log("Running in 3000");
 })
