0353228eeec2f6a3b6d56737745548cb3a821cb5
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-07 10:33:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ const db = mysql.createPool({
   database: 'learner1',
   user: 'learner1',
   password: 'learner@123'
-})
+});
 
 
 db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
+});
+
+app.post("/submit",(req,res)=>{
+  console.log("calling");
+});
+
+
+app.listen(3000,() => {
+  console.log("RUnning in 3000");
 });
