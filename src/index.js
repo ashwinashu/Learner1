52f196853a3856522335887824bad0fe0a645e13
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-07 18:20:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,27 +31,28 @@ db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 
 
 
-app.get("/show",(req,res)=>{
- console.log("calling");
-  const query1 = "select * from append";
-  db.query(query1,(err,result)=>{
-  res.send(result);
-  console.log(result);
-});
+// app.get("/show",(req,res)=>{
+//  console.log("calling");
+//   const query1 = "select * from append";
+//   db.query(query1,(err,result)=>{
+//   res.send(result);
+//   console.log(result);
+// });
 
 
-app.post("/submit",(req,res)=>{
-  const name = req.body.name;
+// app.post("/submit",(req,res)=>{
+//   const name = req.body.name;
   
- console.log("calling");
-  const query1 = "insert into append values (?)";
-  db.query(query1,[name],(err,result)=>{
-  console.log(result);
-  })
+//  console.log("calling");
+//   const query1 = "insert into append values (?)";
+//   db.query(query1,[name],(err,result)=>{
+//   console.log(result);
+//   })
         
-});
+// });
 
 
-app.listen(4000,() => {
-  console.log("RUnning in 4000");
-});
+// app.listen(4000,() => {
+//   console.log("RUnning in 4000");
+// });
+
