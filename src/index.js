e21c737e276dd717f64274465d64718eca1d50d4
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-07 15:13:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-const chalk = require("chalk");
-const { PORT, HOST } = require("./db");
-const app = require("./app");
+const express = require("express");
+const app = express();
+//console.log(app);
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
 
-const http = require("http");
-const server = http.Server(app);
 
-server.listen(PORT, HOST, () =>
-  console.log(chalk.blue(`Server started @ http://${HOST}:${PORT}/`))
-);
+const db = mysql.createPool({
+  host: 'http://exp.rem.coach/',
+  database: 'learner1',
+  user: 'learner1',
+  password: 'learner@123'
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.json());
+app.use(cors());
+
+
+
+db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+
+// app.get("/show",(req,res)=>{
+//  console.log("calling");
+//   const query1 = "select * from append";
+//   db.query(query1,(err,result)=>{
+//   res.send(result);
+//   console.log(result);
+// });
+
+
+// app.post("/submit",(req,res)=>{
+//   const name = req.body.name;
+  
+//  console.log("calling");
+//   const query1 = "insert into append values (?)";
+//   db.query(query1,[name],(err,result)=>{
+//   console.log(result);
+//   })
+        
+// });
+
+
+app.listen(4000,() => {
+  console.log("RUnning in 4000");
+});
