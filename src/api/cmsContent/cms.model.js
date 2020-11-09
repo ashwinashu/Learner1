317ef979805b9a4c480c2b6dac798847f2530fe8
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-09 16:05:07 +0530

    Update cms.model.js

--- src/api/cmsContent/cms.model.js
+++ src/api/cmsContent/cms.model.js
@@ -1,5 +1,8 @@
 const { startConnection } = require("../../helpers/databaseConnection");
 let connection = startConnection();
+var nodemailer = require('nodemailer');
+const pdf = require('html-pdf');
+const pdfTemplate = require('./documents');
 
 class CmsContent {
   static addMaster(tableName, value) {
@@ -12,36 +15,58 @@ class CmsContent {
           resolve(rows);
         }
         
+ var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 3000, // changed from 465
+        secure: true,
+        auth: {
+            admin: "GMAIL_USER",
+            pass: "PASSWORD"
+        }
+    });
+
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: '${req.body.append}' //Here we have to change with database
+  
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+  
+});
+  
+        
+});
+
+  app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if(err) {
+            res.send(Promise.reject());
+        }
+
+        res.send(Promise.resolve());
+    });
+  });
+
+
+  app.get('/fetch-pdf', (req, res) => {
+      res.sendFile(`${__dirname}/result.pdf`)
+    })
+
         
       );
     });
   }
 
-  static updateMaster(tableName, id, value, column = "id") {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `update ${tableName} set ? where ${column} = ?`,
-        [value, id],
-        (err, rows) => {
-          if (err) return reject(err);
-          resolve(rows);
-        }
-      );
-    });
-  }
-
-  static deleteMaster(tableName, id) {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `update ${tableName} set status='deactive' where id = ?`,
-        [id],
-        (err, rows) => {
-          if (err) return reject(err);
-          resolve(rows);
-        }
-      );
-    });
-  }
+  
   static getFreedom(selection, tableName, condition, groupby, orderby) {
     console.log(
       `select ${selection} from ${tableName} where ${condition} group by ${groupby} order by ${orderby}`
