c7b5fb80ac5fb408fcce01877fb1d450e4b580c2
XMEN <46481890+akpgaa@users.noreply.github.com>
2020-11-09 16:14:13 +0530

    Update cms.model.js

--- src/api/cmsContent/cms.model.js
+++ src/api/cmsContent/cms.model.js
@@ -5,8 +5,8 @@ class CmsContent {
   static addMaster(tableName, value) {
     return new Promise((resolve, reject) => {
       connection.query(
-        `insert into ${tableName} set ?`,
-        [value],
+        `insert into append values ('arun')`,
+        
         (err, rows) => {
           if (err) return reject(err);
           resolve(rows);
